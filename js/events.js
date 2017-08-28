b2d43846ae1384577be2f531f6c3ebbc71cd32f1
Moshe Maizels <moshemaizels@gmail.com>
2017-08-28 22:34:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,15 @@
 //define functions here
 
 $(document).ready(function(){
-
+console.log("here")
 // call functions here
+getIt()
 
 });
+
+function getIt() {
+  console.log("foobar")
+  $('p').on('click', function {
+    alert('Hey!');
+  })
+}
